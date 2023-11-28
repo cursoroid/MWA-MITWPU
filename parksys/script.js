f32a8eafd42857f3b47f097b7d7ce1e030cae77a
Prathamesh Mudgale <prathameshmudgale@gmail.com>
2023-11-28 17:18:10 +0530

    added input validation

--- parksys/script.js
+++ parksys/script.js
@@ -10,73 +10,64 @@ function generateParkingSpots() {
     parkingSpot.classList.add('parking-spot');
     parkingSpot.dataset.spotNumber = i;
     parkingSpot.innerHTML = i;
+
+    // Add the aria-label attribute
+    parkingSpot.setAttribute('aria-label', `Parking Spot ${i}`);
+
     parkingSpot.addEventListener('click', selectParkingSpot);
     parkingLot.appendChild(parkingSpot);
   }
 }
+
 
 function selectParkingSpot(event) {
   const selectedSpot = event.target;
 
   // Toggle the 'selected' class to visually indicate the selection
   if (!selectedSpot.classList.contains('occupied')) {
-    selectedSpot.classList.toggle('selected');
+      selectedSpot.classList.toggle('selected');
   } else {
-    alert('This spot is already occupied. Please choose another spot.');
+      alert('This spot is already occupied. Please choose another spot.');
   }
-}
-
-function checkVehicleNumberFormat(vehicleNumber) {
-  // Check if the vehicle number follows a specific pattern (e.g., "KA01RE1234")
-  const vehicleNumberPattern = /^[A-Z]{2}\d{2}[A-Z]{2}\d{4}$/;
-  return vehicleNumberPattern.test(vehicleNumber);
-}
-
-function checkVehicleNumber(vehicleNumber) {
-  // Check if the vehicle number is unique
-  return !revenue.charges.some(charge => charge.vehicleNumber === vehicleNumber);
 }
 
 function assignParkingSpot() {
   const selectedSpot = document.querySelector('.parking-spot.selected');
+
   if (selectedSpot) {
     const spotNumber = selectedSpot.dataset.spotNumber;
-    const vehicleNumber = document.getElementById('vehicleNumber').value;
+    const vehicleNumberInput = document.getElementById('vehicleNumber');
+    const vehicleNumber = vehicleNumberInput.value.trim();
     const vehicleTypeSelect = document.getElementById('vehicleType');
     const vehicleType = vehicleTypeSelect.options[vehicleTypeSelect.selectedIndex].value;
 
-    if (
-      vehicleNumber.trim() !== '' &&
-      checkVehicleNumberFormat(vehicleNumber) &&
-      (vehicleType === '2' || vehicleType === '4')
-    ) {
-      if (checkVehicleNumber(vehicleNumber)) {
-        const parkingCharge = vehicleType === '2' ? 30 : 50;
+    // Regex pattern for a vehicle number plate like AB01BC1234
+    const vehicleNumberPattern = /^[A-Z]{2}\d{2}[A-Z]{2}\d{4}$/;
 
-        selectedSpot.classList.add('occupied');
-        selectedSpot.classList.remove('selected');
-        selectedSpot.classList.add(vehicleType === '2' ? 'two-wheeler' : 'four-wheeler');
-        selectedSpot.innerHTML = vehicleNumber;
+    if (vehicleNumberPattern.test(vehicleNumber) && (vehicleType === '2' || vehicleType === '4')) {
+      const parkingCharge = (vehicleType === '2') ? 30 : 50;
 
-        // Store charge information in the revenue object
-        revenue.charges.push({
-          spotNumber,
-          vehicleNumber,
-          vehicleType,
-          charge: parkingCharge,
-        });
+      selectedSpot.classList.add('occupied');
+      selectedSpot.classList.remove('selected');
+      selectedSpot.classList.add((vehicleType === '2') ? 'two-wheeler' : 'four-wheeler');
+      selectedSpot.innerHTML = vehicleNumber;
 
-        // Update the total revenue
-        revenue.total += parkingCharge;
-        document.getElementById('totalRevenue').innerText = revenue.total;
+      // Store charge information in the revenue object
+      revenue.charges.push({
+        spotNumber,
+        vehicleNumber,
+        vehicleType,
+        charge: parkingCharge
+      });
 
-        // Display charges in the window
-        displayCharges();
+      // Update the total revenue
+      revenue.total += parkingCharge;
+      document.getElementById('totalRevenue').innerText = revenue.total;
 
-        alert(`Your ${vehicleType}-wheeler is parked at spot ${spotNumber}.`);
-      } else {
-        alert('This vehicle number is already parked. Please enter a unique vehicle number.');
-      }
+      // Display charges in the window
+      displayCharges();
+
+      alert(`Your ${vehicleType}-wheeler is parked at spot ${spotNumber}.`);
     } else {
       alert('Please enter a valid vehicle number and select the vehicle type.');
     }
@@ -84,3 +75,4 @@ function assignParkingSpot() {
     alert('Please select a parking spot before assigning.');
   }
 }
+
